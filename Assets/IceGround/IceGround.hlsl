af7f788c4fb2e8d9f3fc5d260e551b7f158de9e6
VersyF <1598906114@qq.com>
2026-08-24 20:40:19 +0800

    IG:微调视差逻辑

--- Assets/IceGround/IceGround.hlsl
+++ Assets/IceGround/IceGround.hlsl
@@ -17,21 +17,21 @@ void IceGround_Texture_float
 )
 {
     float3 baseColor = SAMPLE_TEXTURE2D(_MainTex, _SamplerState, _UV);
-    float3 color = 0;
+    float3 color0 = 0;
     
     float3 color_cache;
     float2 viewDir = -float2(_PixelPos.x - _CameraPos.x, _PixelPos.z - _CameraPos.z);        //这里不能normalize！因为需要距离
     for (int i = 0; i < _LoopNum; i++)
     {
-        
-        color_cache = SAMPLE_TEXTURE2D(_MainTex, _SamplerState, _UV);
-        color += color_cache * exp(-i * _Atten) ;                                                           //衰减系数乘在自变量，控制衰减速度；放在外面会影响颜色本身
         _UV += viewDir * _OffScale;
+        color_cache = SAMPLE_TEXTURE2D(_MainTex, _SamplerState, _UV);
+        color0 += color_cache * exp(-i * _Atten) ;                                                           //衰减系数乘在自变量，控制衰减速度；放在外面会影响颜色本身
+        
         
     }
-    color /= _LoopNum;
+    color0 /= _LoopNum;
     
-    output = saturate(lerp(baseColor, color, _Lerp)) ;
+    output = saturate(lerp(baseColor, color0, _Lerp)) ;
     
     return;
 }
